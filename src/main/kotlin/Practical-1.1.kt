89edfce53986f986bcd4b2abf01a43ed68d0c81d
student <student@gnu.ac.in>
2023-07-14 08:27:51 +0530

    Created Practical-1.1

---
+++ src/main/kotlin/Practical-1.1.kt
@@ -0,0 +1,22 @@
+fun main(){
+    var a:Int = 10
+    var b:Float = 1.2F
+    var c:Char = 'A'
+    var d:String = "BRIJESH"
+    var e:Boolean = true
+    var f:Double = 10.20
+    var g:Long = 12345678910
+    var h:Short = -1
+    var i:Byte = 127
+    println("ADESHARA BRIJESH_21012021001")
+    println("Integer value : $a ")
+    println("Float value : $b ")
+    println("Character value : $c ")
+    println("String value : $d ")
+    println("Boolean value : $e ")
+    println("Double value : $f ")
+    println("Long value : $g ")
+    println("Short value : $h ")
+    println("Byte value : $i ")
+
+}
