488d9a260a935b7d3f4699d08cded8bd51a7ec29
student <student@gnu.ac.in>
2023-07-14 08:46:19 +0530

    Created Practical-1.2

---
+++ src/main/kotlin/Practical-1.2.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var a:Int = 10
+    var b:Float = 1.2F
+    var c:Char = 'A'
+    var d:String = "BRIJESH"
+    var e:Boolean = true
+    var f:Double = 10.20
+    var g:Long = 12345678910
+    var h:Short = -1
+    var i:Byte = 127
+
+}
