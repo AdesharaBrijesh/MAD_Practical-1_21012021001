695c987b85fbf1e8ecda102157e61221528e29f9
student <student@gnu.ac.in>
2023-07-18 08:32:47 +0530

    added more files

--- src/main/kotlin/Practical-1.2.kt
+++ src/main/kotlin/Practical-1.2.kt
@@ -1,12 +1,14 @@
-fun main(){
-    var a:Int = 10
-    var b:Float = 1.2F
-    var c:Char = 'A'
-    var d:String = "BRIJESH"
-    var e:Boolean = true
-    var f:Double = 10.20
-    var g:Long = 12345678910
-    var h:Short = -1
-    var i:Byte = 127
 
+fun main(){
+    println("MAD_Practical-1.2_21012021001")
+    var a:Int = 10
+    println("Integer value : $a ")
+    var b:Double = a.toDouble()
+    println("Double value (from Integer value) : $b ")
+    var c:String = "10"
+    println("String value : $a ")
+    var d:Int = c.toInt()
+    println("Integer value (from String value) : $d ")
+    var e:Double = c.toDouble()
+    println("Double value (from String value) : $e ")
 }
---
+++ src/main/kotlin/Practical-1.4.kt
@@ -0,0 +1,9 @@
+import java.util.Scanner
+fun main() {
+    println("MAD_Practical-1.2_21012021001")
+    var reader = Scanner(System.`in`)
+    println("enter the number : ")
+    var x:Int = reader.nextInt()
+    var ans:String = if(x.toInt() %2 == 0) "even" else "odd"
+    println("$x is $ans")
+}
